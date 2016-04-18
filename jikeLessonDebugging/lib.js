27e40dd8098ca4bf8595b5e5396271addfd95b17
guazipi <sam_lhx@126.com>
2016-04-18 15:03:02 +0800

    annotation

--- jikeLessonDebugging/lib.js
+++ jikeLessonDebugging/lib.js
@@ -1,6 +1,7 @@
 /**
  * Created by chet on 16/4/18.
  */
+    //这里存在一个将一个函数作为一个参数传递给一个函数的问题，要熟悉并会用这种写法。
 //var mydiv=document.getElementById("testDiv");
 //mydiv.onclick= function () {
 //    getm(function (data) {
@@ -19,6 +20,9 @@
 //    });
 //}
 
+//这里新学会了一个封装，感觉js中的对象反反复复都是那几个，不过就看谁搞的巧妙一些。
+    //这里算是一个写法吧，可以以后多加应用，熟能生巧。
+    //无论如何是不能再傻傻的写很多function罗列着来回调用了
 var test={
     a:1,
     init: function () {
@@ -31,13 +35,13 @@ var test={
         me.btn=document.getElementById("testDiv");
     },
     bind: function () {
-        this.btn.onclick=this.getm();
+        this.btn.onclick=this.getm;
     },
     getm: function () {
         $.ajax({
             url:"data.php",
             success:function(data){
-                //debugger
+                debugger
                 console.log(data);
                 alert(data);
             }
