0db8ffdb0f8ecedc4cb0f19627e5a62f54509364
chetliu <sam_lhx@126.com>
2016-04-18 14:57:11 +0800

    package

--- jikeLessonDebugging/lib.js
+++ jikeLessonDebugging/lib.js
@@ -1,21 +1,48 @@
 /**
  * Created by chet on 16/4/18.
  */
-var mydiv=document.getElementById("testDiv");
-mydiv.onclick= function () {
-    getm(function (data) {
-        alert(data);
-    });
+//var mydiv=document.getElementById("testDiv");
+//mydiv.onclick= function () {
+//    getm(function (data) {
+//        alert(data);
+//    });
+//};
+//
+//function getm(fn){
+//    $.ajax({
+//        url:"data.php",
+//        success:function(data){
+//            debugger
+//            console.log(data);
+//            fn(data);
+//        }
+//    });
+//}
+
+var test={
+    a:1,
+    init: function () {
+        var me=this;
+        me.bind();
+        me.render();
+        me.render();
+    },
+    render: function () {
+        var me=this;
+        me.btn=document.getElementById("testDiv");
+    },
+    bind: function () {
+        this.btn.onclick=this.getm();
+    },
+    getm: function () {
+        $.ajax({
+            url:"data.php",
+            success:function(data){
+                //debugger
+                console.log(data);
+                alert(data);
+            }
+        });
+    }
 };
-
-function getm(fn){
-    $.ajax({
-        url:"data.php",
-        success:function(data){
-            debugger
-            console.log(data);
-            fn(data);
-        }
-    });
-}
-
+test.init();
